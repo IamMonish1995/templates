af9fbc5154f1ae89027ca3b734ed9af4c80bcefc
Wasif <swasifr@gmail.com>
2022-07-05 10:49:22 +0530

    deleted extra files

--- src/MAIN_URL.js
+++
@@ -1,8 +0,0 @@
-const MAIN_URL = "https://www.universal-tutorial.com/api" // BACKEND MAIN API LINK
-
-export const header = {
-    'Accept': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJtb2huaXNoLmJhcnNlQHBpeG9uaXgudGVjaCIsImFwaV90b2tlbiI6ImVLaU9Gd2JyUTg2SWhyZ2tUcmViekpOWExrRkFpamdNUlgzRVhzU0JiZjFDdmFMcVZocUZ1dkdQRnRQM1RNcmM1M2cifSwiZXhwIjoxNjU3MDA4NjM0fQ.75BQjvqBsJqQwFvF9nPZczJl5AQb1SY4dDYAaRxIePU'
-}
-
-export default MAIN_URL;
